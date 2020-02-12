a033f108ae6ce40092df69b0a2662bc7994e25e8
kingjulian31405 <kingjulian314@gmail.com>
2020-02-12 19:53:46 +0000

    added random function

--- nimbot.py
+++ nimbot.py
@@ -1,16 +1,17 @@
 import os
-
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='nim')
 async def play_nim(ctx):
-    await ctx.send("Still working on it, kiddo.")
+    await ctx.send("Still working on it, kiddo")
 
 bot.run(token)
+
+def random(x):
+    print(x)
 
