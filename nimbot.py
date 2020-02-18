7df79e00364977db71d6486637b5f7d4802f45fd
kingjulian31405 <kingjulian314@gmail.com>
2020-02-18 19:50:57 +0000

    added flip coin function to nim_bot

--- nimbot.py
+++ nimbot.py
@@ -9,6 +9,9 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='nim')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo")
+@bot.command(name='flip_coin')
+async def flip_coin(ctx):
+    await ctx.send(randomchoice(['Heads','Tails']))
 
 bot.run(token)
 
